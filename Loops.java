e7a057c1e81167d588d4b966d6f64261c7aa30a0
Muneer Ul Hassan Bhat <116891395+Itzmuneerbhat@users.noreply.github.com>
2025-01-17 11:58:11 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,27 @@
+public class Loops {
+    public static void main(String[] args) {
+        //this is for loop
+// for(int i=0; i>100; i++)
+       for(int counter = 0 ; counter < 10; counter ++){
+           System.out.println(counter+" " );
+       }
+     //this is while loop
+      //   int i = 0 ;
+        //  while (i<100) {
+        //    System.out.println(i);
+       //     i++ ;
+            
+        //  }
+
+        // this is do while loop
+        //  int i = 0;
+
+        //  do{
+            // System.out.println(i);
+      //  i++ ;
+        // }
+        // while( i<10);
+        
+        
+    }
+}

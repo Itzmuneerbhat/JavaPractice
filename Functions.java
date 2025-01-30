dae830b5a26300c354bc6e9f9014a680488839c6
Muneer Ul Hassan Bhat <116891395+Itzmuneerbhat@users.noreply.github.com>
2025-01-30 12:01:33 +0530

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,19 @@
+public class Functions {
+
+public static int product(int a , int b){
+   int multiply = a*b;
+    return multiply;
+
+}
+
+public static void main(String []args){
+int a = 10 ;
+int b = 5;
+int multiply=product(a, b);
+  System.out.println(multiply);
+    }
+
+
+}
+    
+

928cba109e5acf508f6d61313b69ac933ca7fe01
itzmuneerbhat <muneerbhat2002@gmail.com>
2025-02-16 21:08:36 +0530

    solid rhombus pattern

---
+++ solidRhombus.java
@@ -0,0 +1,27 @@
+public class solidRhombus {
+
+    public static void solidRhombus(int n){
+
+        for(int i = 1 ; i <=n ; i++){
+            // spaces
+            for(int j = 1 ; j<=(n-i); j++){
+                System.out.print(" ");
+
+            }
+            // stars
+            for(int j = 1; j<=n ; j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+           
+
+
+        }
+    }
+    
+    public static void main(String[] args) {
+        solidRhombus(5);
+        
+    }
+}
